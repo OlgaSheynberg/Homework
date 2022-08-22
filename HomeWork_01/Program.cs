a8f6ae64b257bb82dc0681ee4a4a2b167e3380b4
Olga Sh <shei-olya@mail.ru>
2022-08-23 00:43:57 +0300

    Практическоое задание_Задача№2

---
+++ HomeWork_01/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+Console.WriteLine("Введите число a:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = int.Parse(Console.ReadLine());
+int max = 0;
+int min = 0;
+if (a > b)
+{
+    max = a;
+    min = b; 
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("max=");
+Console.WriteLine(max);
+Console.WriteLine("min=");
+Console.WriteLine(min);
