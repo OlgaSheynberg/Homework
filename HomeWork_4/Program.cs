6ebcca3861ee93caf4acb7b2524f656215d68dea
Olga Sh <shei-olya@mail.ru>
2022-08-23 16:37:38 +0300

    Практическоое задание_Задача№4

---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine(" Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
+
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+Console.WriteLine("Чётные числа от 1 до N:");
+
+while (i<=N)
+{
+    int b = (i%2);
+     if (b==0)
+{
+    Console.WriteLine(i);
+}
+    i++;
+}
