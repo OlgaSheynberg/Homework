81dabd1304981e16a850a66b280de5fe0865cbfa
Olga Sh <shei-olya@mail.ru>
2022-08-23 16:18:55 +0300

    Практическоое задание_Задача№2

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine(" Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+Console.WriteLine("Введите число a:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b:");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число с:");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+    if (b > a)
+{
+    max = b;
+    
+}
+
+    if (c > b)
+    {
+        max = c;
+    }
+
+
+Console.WriteLine("max=");
+Console.WriteLine(max);
