e81510413af4bf36f957db0ef298ad3c815f5d62
Olga Sh <shei-olya@mail.ru>
2022-08-23 16:11:54 +0300

    Практическоое задание_Задача№3

---
+++ HomeWork_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).");
+
+Console.WriteLine("Введите число a:");
+int a = int.Parse(Console.ReadLine());
+
+int b = (a%2);
+
+if (b==0)
+{
+   Console.WriteLine(a);
+   Console.WriteLine("чётное");
+}
+else
+{
+   Console.WriteLine(a);
+   Console.WriteLine("нечётное");
+}
